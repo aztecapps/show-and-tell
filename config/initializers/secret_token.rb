461526bfb51d8a353375e9d9eb38454f8cf53c3f
root <root@aztecapps.(none)>
2013-05-21 04:34:50 +0000

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowAndTell::Application.config.secret_token = 'b1a1c65dd40e495149bed5d55b61b4db4f92044b35ffd4a145c04a451e4ca298fb99b3152d5e6fcce07484af6a1fc9f7cb76a6e87c113212c677445466c9d6e6'
